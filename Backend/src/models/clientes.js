97823631485fc629c88292b389bf60df50021e26
fernandoox30 <20230453@ricaldone.edu.sv>
2026-06-27 01:16:45 -0600

    Backend primero commit

---
+++ Backend/src/models/clientes.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const clientesSchema = new Schema(
+  {
+    name: { type: String },
+    lastName: { type: String },
+    birthdate: { type: Date },
+    email: { type: String },
+    password: { type: String },
+    isVerified: { type: Boolean },
+    loginAttemps: { type: Number },
+    timeOut: { type: Date },
+  },
+  {
+    timestamps: true,
+    strict: false,
+  },
+);
+
+export default model("Clientes", clientesSchema);
